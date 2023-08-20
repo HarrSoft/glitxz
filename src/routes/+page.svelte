55253eed6d7b30e086a2be4afa5c34473ade4008
Ahmed Zaher <contact@ahmedzaher.net>
2023-08-20 09:12:54 -0600

    clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="p-10">
-  <h1 class="font-black font-navigo text-2xl">Who are we?</h1>
-  <p class="max-w-[80%] font-navigo">
+  <h1 class="font-black text-2xl">Who are we?</h1>
+  <p class="max-w-[80%]">
     <strong>Glitxz -</strong> A collective of artists, gamers, fashion scholars, and creators from diverse backgrounds
     who have come together to explore the intersection between Art, Fashion, and Gaming.
   </p>
--- src/routes/components/nav/navMainTopic.component.svelte
+++ src/routes/components/nav/navMainTopic.component.svelte
@@ -1,14 +1,12 @@
-<script>
+<script lang="ts">
 	import { page } from "$app/stores";
 
-  export let activeColor = 'black';
-  /**
-	 * @type {string}
-	 */
-   export let href;
+  export let activeColor:string = 'black';
+  export let href:string = '/';
 </script>
+
 <div class="m-5 my-10">
-  <a href={href} class:active="{$page.url.pathname.includes(href)}" class={`font-black text-2xl font-navigo text-${activeColor}`}>
+  <a href={href} class:active="{$page.url.pathname.includes(href)}" class={`font-black text-2xl text-${activeColor}`}>
       <slot/>
   </a>
 </div>
