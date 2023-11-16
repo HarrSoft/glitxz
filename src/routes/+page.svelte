5748c98b165b1d938e59528f9dbee5137c45e7ce
Lavra Tamutus <lavratamutus@gmail.com>
2023-11-15 23:53:17 -0500

    Add seemingly important session helper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,47 @@
-<div class="p-10 max-w-[80%]">
-  
-  <section>
-    <h1 class="font-black text-2xl my-10">Who are we?</h1>
-    <p class="max-w-[80%]">
-      <strong>Glitxz -</strong> A collective of artists, gamers, fashion scholars, and creators from diverse backgrounds
-      who have come together to explore the intersection between Art, Fashion, and Gaming.
-    </p>
-  </section>
+<div class="max-w-[80%] p-10">
+	<section>
+		<h1 class="my-10 text-2xl font-black">Who are we?</h1>
+		<p class="max-w-[80%]">
+			<strong>Glitxz -</strong> A collective of artists, gamers, fashion scholars, and creators from
+			diverse backgrounds who have come together to explore the intersection between Art, Fashion, and
+			Gaming.
+		</p>
+	</section>
 
-  <section>
-    <h1 class="font-black text-2xl mt-16 my-10">Why?</h1>
-    <p>
-      By exploring these interconnected yet separate concepts, we hope to improve the day to day experience <br/> 
-      of all conscious beings. We will build experiences that make a world that people are genuinely thrilled <br/>
-      to live in. <br/><br/>
-  
-      Our genuine belief is that we can use these ideas as tools. <br/><br/>
-    </p>
-    <ul class="list-none">
-      <li>
-        Tools that resolve conflict by deepening understanding
-      </li>
-      <li>
-        Tools that gamify economic productivity
-      </li>
-      <li>
-        Tools that reinvent society itself
-      </li>
-      <li>
-        Tools with limitless untapped potential 
-      </li> 
-    </ul>
-  </section>
-  
-  <section>
-    <h1 class="font-black text-2xl my-16">What we mean when we say those words.</h1>
-    <div class="flex w-full justify-between">
-      <div>
-        <h1 class="font-black text-2xl">Art.</h1>
-        <p>Art is a experience captured crystalized and shared</p>
-      </div>
-      <div>
-        <h1 class="font-black text-2xl">Fashion.</h1>
-        <p>Fashion is how we share with the world how we experience ourselves</p>
-      </div>
-      <div>
-        <h1 class="font-black text-2xl">Gaming.</h1>
-        <p>Games are curated structured  interactive experiences</p>
-      </div>
-    </div>
-  </section>
+	<section>
+		<h1 class="my-10 mt-16 text-2xl font-black">Why?</h1>
+		<p>
+			By exploring these interconnected yet separate concepts, we hope to improve the day to day
+			experience <br />
+			of all conscious beings. We will build experiences that make a world that people are genuinely
+			thrilled <br />
+			to live in. <br /><br />
 
+			Our genuine belief is that we can use these ideas as tools. <br /><br />
+		</p>
+		<ul class="list-none">
+			<li>Tools that resolve conflict by deepening understanding</li>
+			<li>Tools that gamify economic productivity</li>
+			<li>Tools that reinvent society itself</li>
+			<li>Tools with limitless untapped potential</li>
+		</ul>
+	</section>
 
+	<section>
+		<h1 class="my-16 text-2xl font-black">What we mean when we say those words.</h1>
+		<div class="flex w-full justify-between">
+			<div>
+				<h1 class="text-2xl font-black">Art.</h1>
+				<p>Art is a experience captured, crystalized, and shared</p>
+			</div>
+			<div>
+				<h1 class="text-2xl font-black">Fashion.</h1>
+				<p>Fashion is how we share with the world how we experience ourselves</p>
+			</div>
+			<div>
+				<h1 class="text-2xl font-black">Gaming.</h1>
+				<p>Games are curated, structured, interactive experiences</p>
+			</div>
+		</div>
+	</section>
 </div>
