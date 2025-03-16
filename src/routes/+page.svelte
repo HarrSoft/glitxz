186a7a0fb98ba2da7ce33f4ae215171c4439e985
cano392 <cano19111+2@gmail.com>
2025-03-17 01:08:36 +0800

    update conentComponent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,59 @@
 <script lang="ts">
-	import MainScene from '$lib/components/3D/MainScene.svelte';
 </script>
 
-<MainScene />
+<div class="p-6 pt-12">
+	<div class="flex flex-col justify-between md:flex-row md:items-start">
+		<div class="flex-1 md:pr-8">
+			<h1 class="mb-6 text-4xl font-bold md:text-6xl">Who are we?</h1>
+			<p class="mb-10 text-lg">
+				Stasoz- A collective of artists, gamers, fashion scholars, and creators from diverse
+				backgrounds who have come together to explore the intersection between Art, Fashion, and
+				Gaming.
+			</p>
+
+			<h2 class="mb-6 text-4xl font-bold md:text-6xl">Why?</h2>
+			<p class="mb-6 text-lg">
+				By exploring these interconnected yet separate concepts, we hope to improve the day to day
+				experience of all conscious beings. We will build experiences that make a world that people
+				are genuinely thrilled to live in.
+			</p>
+
+			<p class="mb-6 text-lg">Our genuine belief is that we can use these ideas as tools.</p>
+
+			<div class="text-lg">
+				<p>Tools that resolve conflict by deepening understanding</p>
+				<p>Tools that gamify economic productivity</p>
+				<p>Tools with limitless untapped potential</p>
+				<p>Tools that reinvent society itself</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="pt-20">
+		<h2 class="mb-10 text-3xl font-bold md:text-5xl">
+			What we mean when we say<br />
+			those words.
+		</h2>
+
+		<div class="grid grid-cols-1 gap-8 md:grid-cols-3">
+			<div>
+				<h3 class="mb-4 text-2xl font-bold md:text-3xl">Fashion</h3>
+				<p class="text-lg">Fashion is how we share with the worlds how we experience ourselves</p>
+			</div>
+
+			<div>
+				<h3 class="mb-4 text-2xl font-bold md:text-3xl">Art</h3>
+				<p class="text-lg">Art is a experience captured crystalized and shared</p>
+			</div>
+
+			<div>
+				<h3 class="mb-4 text-2xl font-bold md:text-3xl">Gaming</h3>
+				<p class="text-lg">Games are curated structured interactive experiences</p>
+			</div>
+		</div>
+
+		<div class="mt-10 text-right">
+			<a href="/" class="border-b border-white text-lg">Join The Movement</a>
+		</div>
+	</div>
+</div>
